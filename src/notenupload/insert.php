3866ea49718e7b4c8218feab84e333d0c0d6f18c
Roger Metzger <roger.metzger@stud.hslu.ch>
2021-03-17 20:56:12 +0100

    Fix noten upload. wrong slashes for linux and remove useless echo

--- src/notenupload/insert.php
+++ src/notenupload/insert.php
@@ -1,18 +1,18 @@
 <?php
 $orgdoc = new DOMDocument;
-$orgdoc->load('uploads\Noten.xml');
+$orgdoc->load('uploads/Noten.xml');
 
 $node = $orgdoc->getElementsByTagName("Prüfung")->item(0);
 
 $newdoc = new DOMDocument;
 $newdoc->preserveWhiteSpace = false;
 $newdoc->formatOutput = true;
-$newdoc->load('..\database\Noten-DB.xml');
+$newdoc->load('../database/Noten-DB.xml');
 
 // Import the node, and all its children, to the document
 $node = $newdoc->importNode($node, true);
 // And then append it to the "<Prüfungen>" node
 $newdoc->documentElement->appendChild($node);
 
-$newdoc->save('..\database\Noten-DB.xml');
+$newdoc->save('../database/Noten-DB.xml');
 ?>
